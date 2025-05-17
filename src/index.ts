a611a77482db3fe204bfad01b5efe648e0447c43
kraevskiy <dsik-web@ukr.net>
2025-05-17 14:09:10 +0300

    add server listener

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import 'dotenv/config';
+import express from 'express'
 
 import { init } from './init.js';
+const app = express()
 
 init();
+const port = process.env.PORT || 4000
 
-export {};
+app.get('/', (req, res) => {
+	res.send('Hello World')
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
